f4dd0c869010f1ee12546cb0079b2b2aad9346b6
maoger <maoyantsing@gmail.com>
2016-10-25 12:40:41 +0800

    Update Copyer.js

--- Copyer.js
+++ Copyer.js
@@ -2,14 +2,14 @@
 // @name         Copyer
 // @namespace    http://tampermonkey.net/
 // @version      1.0
-// @description  恢复复制粘贴功能
+// @description  恢复被网站禁用的复制粘贴功能
 // @author       maoger
 // @match        http://*.gov.cn/*
 // @grant        none
 // ==/UserScript==
 
 //免责声明：本代码仅供技术交流，请勿用于商业及非法用途，如产生法律纠纷与本人无关。
-//目前仅发现“全国企业信用信息公示系统（浙江）”不能复制粘贴，仅修改body中的onselectstart属性即可恢复复制粘贴功能。
+//适用：“全国企业信用信息公示系统（浙江）”，原网站设置了不能复制粘贴，通过修改body中的onselectstart属性即可恢复复制粘贴功能。
 
 (function() {
     'use strict';
