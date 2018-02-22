5f90333067d3f77d87cbd0ee7a404f02c108cf92
maoger <maoyantsing@gmail.com>
2018-02-22 19:47:05 +0800

    update

--- Copyer.user.js
+++ Copyer.user.js
@@ -3,13 +3,11 @@
 // @namespace    http://tampermonkey.net/
 // @homepage     https://github.com/maoger/Copyer
 // @version      1.2
-// @description  恢复被网站禁用的复制粘贴功能；最近工商信息网站更新，所有网站已均可复制粘贴。
+// @description  恢复被网站禁用的复制粘贴功能。
 // @author       maoger
-// @match        http://gsxt.zjaic.gov.cn/*
+// @include      http*://*
 // @updateURL    https://openuserjs.org/meta/maoger/Copyer.meta.js
 // ==/UserScript==
-
-//免责声明：本代码仅供技术交流，请勿用于商业及非法用途，如产生法律纠纷与本人无关。
 
 (function() {
     'use strict';
